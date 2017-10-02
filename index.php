e22c234aad51e547b52523887ce48dbf4a1331fd
halsaylani <hasooo7y@hotmail.com>
2017-10-02 16:19:55 -0400

    removing the unusfull spaces

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 
 <h1/> welcome to the website<h1 /h1>
-
 <html>
-
 <form method="POST" action="try.php">
-
   <label for="username">Username</label>
   <input type="username"  name="username"><br>
-   
-
   Password <input type="text" name="password"><br>
-
   <button type = "submit">Login</button>
-  
-  
 </form>
 </html>
 
