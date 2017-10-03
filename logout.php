4d907950e02122aec951838a789c49ddd007a3bf
halsaylani <hasooo7y@hotmail.com>
2017-10-02 23:05:35 -0400

    update
    change the text after user logout

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	session_destroy();
-	echo "I hope you enjoy our website " . $_SESSION['uname'];
+	echo "see you again " . $_SESSION['uname'];
 	echo "<p><a href= 'index.php'> Press here to login again</a>";
 	
 
