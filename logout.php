8241098829b35856952cc38e4b7d0b5b8bb96b5c
halsaylani <hasooo7y@hotmail.com>
2017-10-02 21:10:33 -0400

    updating
    update the try file,storing the name and the nuber into array, and trying to active the attempts

--- logout.php
+++ logout.php
@@ -3,5 +3,6 @@
 	session_destroy();
 	echo "see you again " . $_SESSION['uname'];
 	echo "<p><a href= 'index.php'> Press here to login again</a>";
-	
+	$_SESSION[];
+	echo $_SESSION;
 ?>
--- try.php
+++ try.php
@@ -1,33 +1,41 @@
      <?php
      $uname=$_POST['uname'];
      $pass=$_POST['pass'];
-     //
+     $arrayuname=("hussan");
+     $arraypass=("123321");
+     $_SESSION['attempts']=0;
+    
      if(isset($_POST['submit'])){
-     if($uname == "hussan"){
-     if($pass == "123321"){
+     if($uname == $arrayuname){
+     if($pass == $arraypass){
      
      session_start();
      $_SESSION['uname'] = $uname;
+     $_SESSION['pass'] = $pass;
+     $_SESSION['login']=true;
+     
      //session_start();	
      echo "welcome " . $_SESSION['uname']."<br>";
      echo " password is: " . $_SESSION['pass']."<br>";
      echo " Today is " . date("Y/m/d") . "<br>";
-     echo "<p><a href= ‘logout.php'> Press here to logout</a>";
+     //echo "count" .$_SESSION['attempts'];
+     echo "<p><a href= 'logout.php'> logout</a>";
      
      }
      
      else{
       echo "Invalid Password";
-	echo "<p><a href= 'index.php'> try again</a>";
+      echo "<p><a href= 'index.php'> try again</a>";
      
      }
      
      }
      else{
      echo "Invalid Username";
-	echo "<p><a href= 'index.php'> try again</a>";
+     echo "<p><a href= 'index.php'> try again</a>";
     
      }
+     
      }
      
      ?>
