7ed98c235d0877bf0bf6bfc7067fbb6c414026be
halsaylani <hasooo7y@hotmail.com>
2017-10-02 22:14:38 -0400

    update the try file
    removing unusfull variables and adding the function for the attempts

--- try.php
+++ try.php
@@ -1,29 +1,28 @@
      <?php
+     session_start();
+     //global $count=0;
      $uname=$_POST['uname'];
      $pass=$_POST['pass'];
      $arrayuname=("hussan");
      $arraypass=("123321");
-     $_SESSION['attempts']=0;
     
      if(isset($_POST['submit'])){
      if($uname == $arrayuname){
      if($pass == $arraypass){
-     
-     session_start();
+   
+
      $_SESSION['uname'] = $uname;
      $_SESSION['pass'] = $pass;
-     $_SESSION['login']=true;
-     
-     //session_start();	
+    
+ 	
      echo "welcome " . $_SESSION['uname']."<br>";
      echo " password is: " . $_SESSION['pass']."<br>";
      echo " Today is " . date("Y/m/d") . "<br>";
-     //echo "count" .$_SESSION['attempts'];
      echo "<p><a href= 'logout.php'> logout</a>";
      
      }
      
-     else{
+     else{ 
       echo "Invalid Password";
       echo "<p><a href= 'index.php'> try again</a>";
      
@@ -31,11 +30,16 @@
      
      }
      else{
-     echo "Invalid Username";
+     echo  "Invalid Username";
      echo "<p><a href= 'index.php'> try again</a>";
     
      }
      
      }
+	if(isset( $_POST['attempts'])){
+     $_SESSION['attempts'] = $_SESSION['attempts'] +1;
+     echo "attempts are ".$_SESSION['attempts']; 
+     echo "<br>"."Invalid UserName/Password";
+}
      
      ?>
