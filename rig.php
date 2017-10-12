66f134cf42fab7ee4faa76fc98924b89dc281995
halsaylani <hasooo7y@hotmail.com>
2017-10-12 19:04:19 -0400

    Registration
    adding sing up page and ask for information from user

---
+++ rig.php
@@ -0,0 +1,13 @@
+
+<h1/> sing up<h1 /h1>
+<html>
+<body>
+<form method="post" action="rig.php">
+  Username <input type="username"  name="uname"><br>
+  Password <input type="text" name="pass"><br/>
+  Repassword <input type="text" name="pass2"><br/>
+  email <input type="text" name="email"><br/>
+   <input type = "submit" name = "rig" value = "Register">
+</form>
+</body>
+</html>
