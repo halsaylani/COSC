3eccca33fb3067b2ec9547b2b97df7f661ea58a6
halsaylani <hasooo7y@hotmail.com>
2017-10-13 08:24:48 -0400

    update
    changin a variable name

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	session_destroy();
-	echo "see you again " . $_SESSION['uname'];
+	echo "see you again " . $_SESSION['name'];
 	echo "<p><a href= 'index.php'> Press here to login again</a>";
 	
 
