d5a6c0026ae5c9716f9e40767829ce7213f4e562
halsaylani <hasooo7y@hotmail.com>
2017-10-13 17:53:24 -0400

    add
    adding the is authenticated if its not ho to home page

--- welcome.php
+++ welcome.php
@@ -1,10 +1,16 @@
 <?php
-SESSION_start();
-	 echo "welcome " . $_SESSION['name']."<br>";
-     //echo " password is: " . $_SESSION['pass']."<br>";
+session_start();
+
+
+if(isset($_SESSION['name'])){
+    echo "welcome " . $_SESSION['name']."<br>";
      echo " Today is " . date("Y/m/d") . "<br>";
      echo "<p><a href= 'logout.php'> logout</a>";
-     //if(!isset($_SESSION['is authenticated'])){
-     //header("location:index.php");
-     //}
+
+}else{
+    header("location:index.php");
+}
+      if(!isset($_SESSION['is authenticated'])){
+      header("location:index.php");
+      }
 ?>
