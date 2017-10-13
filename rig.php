4f698a5beae4c505d5bd55e7ce697fa069e9874e
halsaylani <hasooo7y@hotmail.com>
2017-10-12 23:42:23 -0400

    update
    updating the rig files where user know can sing up his/her info to my data base

--- rig.php
+++ rig.php
@@ -1,27 +1,35 @@
-
- <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+<?php
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    $conn = new PDO('mysql:127.0.0.1;=$servername;dbname=COSC', 'root', '');
+    if(isset($_POST['rig'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $pass=$_POST['pass'];
+    $insert=$conn->prepare("INSERT INTO users(username, password, email)
+    						 values(:name,:pass,:email)");
+    $insert->bindParam('name',$name);
+    $insert->bindParam('email',$email);
+    $insert->bindParam('pass',$pass);
+    $insert->execute();
+    header("location:index.php");
+    
+    }
+    
     }
 catch(PDOException $e)
     {
-    echo "Connection failed: " . $e->getMessage();
+    echo $sql . "<br>" . $e->getMessage();
     }
+    $conn = null;
 ?> 
-<h1/> sing up form <h1 /h1>
+
+<h1/> sing up<h1 /h1>
 <html>
 <body>
 <form method="post" action="rig.php">
-  Username <input type="username"  name="uname"><br>
+  Username <input type="username"  name="name"><br>
   Password <input type="text" name="pass"><br/>
-  Repassword <input type="text" name="pass2"><br/>
   email <input type="text" name="email"><br/>
    <input type = "submit" name = "rig" value = "Register">
 </form>
