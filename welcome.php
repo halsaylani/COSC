d1d9a9631aa6f11f98ffa30f04d2a4a96d334330
halsaylani <hasooo7y@hotmail.com>
2017-10-12 23:44:55 -0400

    update

--- welcome.php
+++ welcome.php
@@ -1,6 +1,6 @@
 <?php
 SESSION_start();
-	 echo "welcome " . $_SESSION['uname']."<br>";
+	 echo "welcome " . $_SESSION['name']."<br>";
      echo " password is: " . $_SESSION['pass']."<br>";
      echo " Today is " . date("Y/m/d") . "<br>";
      echo "<p><a href= 'logout.php'> logout</a>";
