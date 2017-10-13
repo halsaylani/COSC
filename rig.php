491ac374e7c400f7aa155ffcfd21e0e833c97dd7
halsaylani <hasooo7y@hotmail.com>
2017-10-12 22:26:01 -0400

    sql
    checking if data base is connected

--- rig.php
+++ rig.php
@@ -1,5 +1,21 @@
 
-<h1/> sing up<h1 /h1>
+ <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
+<h1/> sing up form <h1 /h1>
 <html>
 <body>
 <form method="post" action="rig.php">
