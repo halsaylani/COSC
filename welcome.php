dca1f4e7d1da05298bb55d8a1215b1c6812778b3
halsaylani <hasooo7y@hotmail.com>
2017-10-13 08:19:28 -0400

    update

--- welcome.php
+++ welcome.php
@@ -1,10 +1,10 @@
 <?php
 SESSION_start();
 	 echo "welcome " . $_SESSION['name']."<br>";
-     echo " password is: " . $_SESSION['pass']."<br>";
+     //echo " password is: " . $_SESSION['pass']."<br>";
      echo " Today is " . date("Y/m/d") . "<br>";
      echo "<p><a href= 'logout.php'> logout</a>";
-     if(!isset($_SESSION['is authenticated'])){
-     header("location:index.php");
-     }
+     //if(!isset($_SESSION['is authenticated'])){
+     //header("location:index.php");
+     //}
 ?>
