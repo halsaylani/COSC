419a8a41890fd8598782b2b71744aec70b5bce2d
halsaylani <hasooo7y@hotmail.com>
2017-10-23 23:31:12 -0400

    update
    controllers home.php file

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,13 +5,9 @@ class Home extends Controller {
     public function index($name = '') {		
         $user = $this->model('User');
 		
-		if (strtolower($_SESSION['name']) == 'mike') {
-			$message = 'You are awesome';
-		} else {
-			$message = 'You suck';
-		}
 		
-        $this->view('home/index', ['message' => $message]);
+		
+        $this->view('home/index');
     }
 
     public function login($name = '') {
