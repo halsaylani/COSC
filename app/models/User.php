de5c22c6cf43df978564bdef7b17c196987b3133
halsaylani <hasooo7y@hotmail.com>
2017-10-30 03:12:21 -0400

    update
    updatting the user model file adding the try and catch to the function authentication and regiter,changing the pass1 variable into pass avoiding troubles

--- app/models/User.php
+++ app/models/User.php
@@ -15,8 +15,9 @@ class User {
          * if username and password good then
          * $this->auth = true;
          */
-		 
-		$db = db_connect();
+		 try {
+    $db = new PDO('mysql:127.0.0.1;=$servername;dbname=COSC', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $query="SELECT * FROM users WHERE username=:name AND password=:pass";
         $statement=$db->prepare($query);
         $statement->execute(array(
@@ -25,20 +26,33 @@ class User {
         ));
         $count=$statement->rowCount();
         if($count>0){
-	$this->auth = true;
+            //$this->auth=true;
             $_SESSION['name']=$_POST['name'];
-			
-		}
+            $_SESSION['auth'] = true;
+        }
     }
-	
-	public function register ($username, $password, $email) {
-		$db = db_connect();
-        $insert=$db->prepare("INSERT INTO users(username, password, email)values(:name,:pass,:email)");
+catch(PDOException $e)
+    {
+    $message = $e->getMessage();
+    }
+    //$db = null;
+
+
+    }
+	public function register ($name, $pass, $email) {
+		try {
+    $db = new PDO('mysql:127.0.0.1;=$servername;dbname=COSC', 'root', '');
+    $insert=$db->prepare("INSERT INTO users(username, password, email)values(:name,:pass,:email)");
     $insert->bindParam('name',$name);
     $insert->bindParam('email',$email);
-    $insert->bindParam('pass',$pass1);
+    $insert->bindParam('pass',$pass);
     $insert->execute();
-
-	}
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+    $db = null;
+}
 
 }
