643092054fa4efc1dcb716c7a15d955b1e0e378f
halsaylani <hasooo7y@hotmail.com>
2017-10-31 01:56:23 -0400

    update
    removing the try and catch from the user file and connect it with the database file with (db_connect),and removing the wrong return form database file

--- app/database.php
+++ app/database.php
@@ -8,7 +8,7 @@ function db_connect() {
     try {
         $dbh = new PDO('mysql:127.0.0.1;=$servername;dbname=COSC', 'root', '');
         return $dbh;
-        return $dbh;
+        
     } catch (PDOException $e) {
 echo $sql . "<br>" . $e->getMessage();
         //We should set a global variable here so we know the DB is down
--- app/models/User.php
+++ app/models/User.php
@@ -15,8 +15,7 @@ class User {
          * if username and password good then
          * $this->auth = true;
          */
-		 try {
-    $db = new PDO('mysql:127.0.0.1;=$servername;dbname=COSC', 'root', '');
+		 $db=db_connect();
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $query="SELECT * FROM users WHERE username=:name AND password=:pass";
         $statement=$db->prepare($query);
@@ -30,29 +29,18 @@ class User {
             $_SESSION['name']=$_POST['name'];
             $_SESSION['auth'] = true;
         }
-    }
-catch(PDOException $e)
-    {
-    $message = $e->getMessage();
-    }
+    
     //$db = null;
-
-
     }
 	public function register ($name, $pass, $email) {
-		try {
-    $db = new PDO('mysql:127.0.0.1;=$servername;dbname=COSC', 'root', '');
+		
+    $db=db_connect();
     $insert=$db->prepare("INSERT INTO users(username, password, email)values(:name,:pass,:email)");
     $insert->bindParam('name',$name);
     $insert->bindParam('email',$email);
     $insert->bindParam('pass',$pass);
     $insert->execute();
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
-    $db = null;
+   
 }
 
 }
