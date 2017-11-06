77c2c73cb3b938a02d328c98f992f1e273e5ec3b
halsaylani <hasooo7y@hotmail.com>
2017-11-06 03:21:25 -0500

    update
    removing the user email,the echo,add the time for blocking the user and removing some useless code

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -3,24 +3,24 @@
 class Login extends Controller {
     public function index() {
         $user = $this->model('User');
-       
+
+        if(!isset($_SESSION['attempts'])){
+     $_SESSION['attempts']=0;
+    }
+    
+        if($_SESSION['attempts']>=3){
+      $locktime=(time() + 60);
+      $_SESSION['lockTime'] = $locktime;
+    }
         if(isset($_POST['submit'])){
       $name=$_POST['name'];
       $pass=$_POST['pass'];
-      $email=$_POST['email'];
       //$checkpass=password_verify($pass,$hash);
-        $user->authenticate($name,$pass,$email);
+        $user->authenticate($name,$pass);
         header('Location: /home');
-    } else{
-          echo "Invalid username/password";
-          $_SESSION['attempts']++;
-          echo "<br>"."attempts are ".$_SESSION['attempts'];
-          echo " after 3 attempts you will be stoped";
-          echo "<p><a href= '../app/views/login.php'> TRY AGAIN</a>";
-    }
-    if($_SESSION['attempts']>3){
-      $_SESSION['attempts']=0;
-      echo" you are out for 60 seconds";
+      }else{
+      $_SESSION['attempts']++;
+
     }
     }
 	public function register () {
@@ -28,16 +28,13 @@ class Login extends Controller {
 		$this->view('home/register');
 		if(isset($_POST['rig'])){
     $name=$_POST['name'];
-    $email=$_POST['email'];
-    $pass1=$_POST['pass'];
-    if($pass1<8){
-      echo 'password should at least contain 8 Characters';
+    $pass=$_POST['pass'];
+    if($pass<8){
+      header("location:/home/error");
       die();
     }
-    $hash=password_hash($pass1,PASSWORD_DEFAULT);
-			$user->register($name,$hash,$email);
-			//$_SESSION['auth'] = true;
-            //header("Location")
+    $hash=password_hash($pass,PASSWORD_DEFAULT);
+			$user->register($name,$hash);
 		}
 	}
 }
