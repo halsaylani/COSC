ee689e59b469e876bbd9d506239043a64c6358a4
halsaylani <hasooo7y@hotmail.com>
2017-11-06 03:23:11 -0500

    error file
    adding error file wher it print the error message for the password

---
+++ app/views/home/error.php
@@ -0,0 +1,9 @@
+<?php require_once '../app/views/templates/header.php' ?>
+
+<div>
+    <div>
+            <p> <?=$data['message']?> </p>
+        </div>
+    </div>
+
+    <?php require_once '../app/views/templates/footer.php' ?>
