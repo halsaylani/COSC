73ba13dcb9244d981919ac5a8a2c26b3efb594ed
halsaylani <hasooo7y@hotmail.com>
2017-11-06 23:49:03 -0500

    update
    the table name from reminder to notes and update the add reminder where user now can add a new reminder based on uyser name,also updating update reminder

--- app/models/Reminders.php
+++ app/models/Reminders.php
@@ -8,7 +8,7 @@ class Reminders {
 	
 	public function get_reminders () {
 		$db = db_connect();
-        $statement = $db->prepare("SELECT * FROM reminder
+        $statement = $db->prepare("SELECT * FROM notes
                                 WHERE username = :name AND deleted = 0;");
         $statement->bindValue(':name', $_SESSION['name']);
 		
@@ -19,33 +19,34 @@ class Reminders {
 	
 	public function get_reminder ($id) {
 		$db = db_connect();
-        $statement = $db->prepare("SELECT * FROM reminder WHERE
+        $statement = $db->prepare("SELECT * FROM notes WHERE
                                 id = :id;");
         $statement->bindValue(':id', $id);
-		
         $statement->execute();
         $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 		return $rows;
 	}
 	public function addReminder($subject,$description) {
 		$db = db_connect();
-    $insert=$db->prepare("INSERT reminder SET subject=:subject , description= :description ");
+    $insert=$db->prepare("INSERT INTO notes (username,subject,description) VALUES(:name,:subject ,:description)");
+    $insert->bindValue(':name', $_SESSION['name']);
     $insert->bindParam('subject',$subject);
     $insert->bindParam('description',$description);
     $insert->execute();
 	}
 	public function updateReminder($id,$subject,$description) {
 		$db = db_connect();
-    $insert=$db->prepare("UPDATE reminder SET subject=:subject , description = :description WHERE id = :id");
-    $statement->execute(array(
-            'subject' => $_POST['subject'],
-            'description' => $_POST['description'],
-        ));
+    $insert=$db->prepare("UPDATE notes SET subject=:subject , description= :description");
+    //$insert->bindValue('name', $username);
+    $insert->bindParam('subject',$subject);
+    $insert->bindParam('description',$description);
+    $insert->execute();
+
 	}
 	
 	public function removeItem($id) {
 		$db = db_connect();
-        $statement = $db->prepare("UPDATE reminder SET deleted = 1 WHERE id = :id");
+        $statement = $db->prepare("UPDATE notes SET deleted = 1 WHERE id = :id");
         $statement->bindValue(':id', $id);
         $statement->execute();
 
