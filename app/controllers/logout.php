2b278267cdfd1a61d887b3139152ea4c1d93e2cd
halsaylani <hasooo7y@hotmail.com>
2017-11-12 18:05:27 -0500

    update
    updatting the logout file where now the file include a logout class and index function that set the time when user log out

--- app/controllers/logout.php
+++ app/controllers/logout.php
@@ -1,4 +1,12 @@
 <?php
+class logout extends Controller{
+	public function index(){
+		$user=$this->model('User');
+		$LastVisit=date('Y-m-d H:i:s');
+		$user->LastVisit($LastVisit);
+		session_destroy();
+	header ('location:/');
+	}
+}
 
-session_destroy();
-header ('location:/');
+?>
