b8fc2793fff9b630c55c2213e56d7f9bf900f7ca
halsaylani <hasooo7y@hotmail.com>
2017-11-13 02:14:01 -0500

    update
    updatting the register view file where now the style of the page been changed

--- app/views/home/register.php
+++ app/views/home/register.php
@@ -1,18 +1,21 @@
 <?php require_once '../app/views/templates/headerPublic.php' ?>
- <head>
-<link rel="stylesheet" type="text/css" href="/css/style.css">
-</head> 
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>sing up here</h1>
-           
+<html>
+<body>
     <form method="post" action="/login/register">
-  Username <input type="username"  name="name"><br>
-  Password <input type="text" name="pass"><br/>
-  email <input type="text" name="email"><br/>
-   <input type = "submit" name = "rig" value = "Register">
-   <a href= "../app/views/login.php"> login</a>
-</form>
+      <fieldset>
+    <legend>sing up here</legend>
+    <div class="form-group">
+      <label for="exampleInputUsername">Username</label>
+      <input class="form-control" placeholder="Username" type="username" name="name">
     </div>
-
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input class="form-control" placeholder="Password" type="password" name="pass">
+    </div>
+    <button type="submit" name = "rig" class="btn btn-primary">Register</button>
+    <a href= "../app/views/login.php"> login</a>
+  </fieldset>
+</form>
+</body>
+</html>
     <?php require_once '../app/views/templates/footer.php' ?>
