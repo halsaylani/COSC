0e487f8165688d49008d17b2cd78bea640e5cbb5
halsaylani <hasooo7y@hotmail.com>
2017-11-13 05:00:41 -0500

    update
    update the footer file where now user can see lastvisit on buttom of the page

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,8 +1,10 @@
 
 <div class="footer">
 	 <p>Copyright &copy; <?php echo date('Y'); ?> </p>
+	 <p>LastVisit: <?=$data['message2']?> </p>
   <h1 class="display-3">Find Us</h1>
   <p>welcome to the website - for any further questions - Please email us exampl@hotmail.com.</p>
+  
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
