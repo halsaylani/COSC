4ae8eab6270b6b3d958158292ab826cf9e2f72bf
halsaylani <hasooo7y@hotmail.com>
2017-11-13 02:09:55 -0500

    update
    the footer file where now the buttom of the page will show some info and some additional futuer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,13 +1,9 @@
-<footer class="footer">    
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
-        </div>
-    </div>
-</footer>
 
+<div class="footer">
+	 <p>Copyright &copy; <?php echo date('Y'); ?> </p>
+  <h1 class="display-3">Find Us</h1>
+  <p>welcome to the website - for any further questions - Please email us exampl@hotmail.com.</p>
 </div>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
 </body>
