41455570e1d78fe91ccc71cbada748c57a6edfc2
halsaylani <hasooo7y@hotmail.com>
2017-11-13 02:08:29 -0500

    update
    updatting the header file where now the header will look like a menue where user can choose where to go from these links

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,42 +4,29 @@ if (isset($_SESSION['auth']) != 1) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <link href= "/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-        <link rel="icon" href="/favicon.png">
-        <title>COSC 4806</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="mobile-web-app-capable" content="yes">
-    </head>
-    <body>
-        <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="/home">COSC</a>
-                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
 
-                <div class="navbar-collapse collapse" id="navbar-main">
-                    <ul class="nav navbar-nav">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">1</a></li>
-                                <li><a href="#">2</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="/logout">Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+
+
+<link href= "/css/style.css" rel="stylesheet" type="text/css"/>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="/home"><?php echo $_SESSION['name']?></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="../app/view/home/index">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/remind/index">Reminders</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/logout/index">Logout</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
