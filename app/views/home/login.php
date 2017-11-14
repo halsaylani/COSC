bca08ab8de71cb25c0fd7905af2cc9534e9e5fbd
halsaylani <hasooo7y@hotmail.com>
2017-11-14 01:16:19 -0500

    update
    adding the pubplic footer to the login and register page

--- app/views/home/login.php
+++ app/views/home/login.php
@@ -3,14 +3,14 @@
 <body>
 <form method="post" action="login/index">
   <fieldset>
-    <legend>welcome to the website</legend>
+    <h1>welcome to the website</h1>
     <div class="form-group">
-      <label for="exampleInputEmail1">Username</label>
-      <input class="form-control" placeholder="Username" type="username" name="name">
+      <label for="exampleUsername">Username</label>
+      <input class="form-contro" placeholder="Username" type="username" name="name">
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
-      <input class="form-control" placeholder="Password" type="password" name="pass">
+      <input class="center-control" placeholder="Password" type="password" name="pass">
     </div>
     <button type="submit" name = "submit" class="btn btn-primary">Login</button>
     <a href= "/login/register"> Create an account</a>
@@ -19,4 +19,4 @@
 </body>
 </html>
 
-    <?php require_once '../app/views/templates/footer.php' ?>
+    <?php require_once '../app/views/templates/footerPublic.php' ?>
--- app/views/home/register.php
+++ app/views/home/register.php
@@ -3,14 +3,14 @@
 <body>
     <form method="post" action="/login/register">
       <fieldset>
-    <legend>sing up here</legend>
+    <h1>sing up here</h1>
     <div class="form-group">
       <label for="exampleInputUsername">Username</label>
-      <input class="form-control" placeholder="Username" type="username" name="name">
+      <input class="form-cotrol" placeholder="Username" type="username" name="name">
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
-      <input class="form-control" placeholder="Password" type="password" name="pass">
+      <input class="fom-control" placeholder="Password" type="password" name="pass">
     </div>
     <button type="submit" name = "rig" class="btn btn-primary">Register</button>
     <a href= "../app/views/login.php"> login</a>
@@ -18,4 +18,4 @@
 </form>
 </body>
 </html>
-    <?php require_once '../app/views/templates/footer.php' ?>
+    <?php require_once '../app/views/templates/footerPublic.php' ?>
