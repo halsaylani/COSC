6dfe88fd60ba33713f7eddcef75ffbfd303ea82e
halsaylani <hasooo7y@hotmail.com>
2017-11-14 01:12:44 -0500

    adding
    adding the footerPublic where now user can see the number of attempts user did on the buttom of the [age

---
+++ app/views/templates/footerPublic.php
@@ -0,0 +1,18 @@
+<?php
+require_once('../app/core/utils.php');
+
+if (isset($_SESSION['auth']) == 1) {
+    header('Location: /home');
+}
+?>
+<div class="footer">
+	 <p>Copyright &copy; <?php echo date('Y'); ?> </p>
+	 <p>Your total attempts are: <?=$data['message']?> </p>
+  <h1 class="display-3">Find Us</h1>
+  <p>welcome to the website - for any further questions - Please email us exampl@hotmail.com.</p>
+  
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
+</body>
+</html>
