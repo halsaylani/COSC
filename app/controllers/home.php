6044f0dd9f13f1fc8a393b2229e2230228964c3b
halsaylani <hasooo7y@hotmail.com>
2017-11-14 01:18:42 -0500

    update
    set the $_SESSION['LasiVisit'] as a message for the user

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,26 +2,30 @@
 
 class Home extends Controller {
 
-    public function index($name = '') {	
-
+    public function index($name = '') {		
         $user = $this->model('User');
-		$message='-welcome '.$_SESSION['name'].'<br>'.
-        '-Today is ' . date("Y/m/d").'/ '.date("l").'<br>'.$_SESSION['LasiVisit'];
+		
+		$message='-welcome '.$_SESSION['name'].'<br>'.'-Today is ' . date("Y/m/d");
+		
         $this->view('home/index', ['message' => $message]);
     }
-    public function LasiVisit($name = '') { 
+     public function LasiVisit($name = '') { 
         $user = $this->model('User');
         $message2=$_SESSION['LasiVisit'];
         $this->view('templates/footer', ['message' => $message2]);
     }
+    public function Attempts($name = '') { 
+        $user = $this->model('User');
+        $message3=$_SESSION['attempts'];
+        $this->view('templates/footerPublic', ['message' => $message3]);
+    }
 
-    public function error() { 
-
+    public function error() {     
         $message='password should contain at least 8 chars';
         $this->view('home/error', ['message' => $message]);
     }
+
     public function login($name = '') {
-        
         $this->view('home/login');
     }
 
