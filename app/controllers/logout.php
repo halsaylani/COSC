cf4dad7126e154f22e88bb6cd57884a0a58e2040
halsaylani <hasooo7y@hotmail.com>
2017-11-20 04:22:51 -0500

    update
    logout file by using the ip addres for the lastvisit

--- app/controllers/logout.php
+++ app/controllers/logout.php
@@ -1,12 +1,11 @@
-<?php
+<?php 
 class logout extends Controller{
 	public function index(){
 		$user=$this->model('User');
-		$LastVisit=date('Y-m-d H:i:s');
-		$user->LastVisit($LastVisit);
+		//$LastVisit=date('Y-m-d H:i:s');
+		$user->LastVisit($_SERVER['REMOTE_ADDR'],$_SESSION['name']);
 		session_destroy();
 	header ('location:/');
 	}
 }
-
 ?>
