35aa0218b70a96e38b3cfa60d4d9ca99575b75d6
halsaylani <hasooo7y@hotmail.com>
2017-11-20 04:16:41 -0500

    adding
    update the report file where four functions been added to the file(mostreminders, remindersdates,totallogins,index

---
+++ app/controllers/report.php
@@ -0,0 +1,38 @@
+<?php
+
+class Report extends Controller {
+    public function index() {
+       
+		 $this->view('report/index');    
+    }
+    public function mostreminders($id='') {
+		 $v = $this->model('Reminders');
+        $list = $v->get_reports();
+        $this->view('report/mostreminders',[
+        'list' => $list
+        ] );   
+    }
+    public function remindersdates($id='') {
+        $v = $this->model('Reminders');
+        
+        if(isset($_POST['find'])){
+            $year1=$_POST['year1'];
+            $year2=$_POST['year2'];
+            $list=$v->findreminders($year1,$year2);
+            $this->view('report/findreminders',[
+        'list' => $list
+        ] );
+
+        }else{
+            $this->view('report/remindersdates');  
+        }
+    }
+    public function totallogins() {
+        $v = $this->model('Reminders');
+        $list=$v->totallogins();
+        $this->view('report/totallogins',[
+        'list' => $list
+        ] );
+   
+    }
+}
