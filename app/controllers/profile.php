fe48bc08daa25606487caad721bc9ec0b62f95b6
halsaylani <hasooo7y@hotmail.com>
2017-11-27 03:24:41 -0500

    adding
    adding a profile controller where all the required informations have some rules to make suer that the information is correct.checking if email is valid and the age of the user

---
+++ app/controllers/profile.php
@@ -0,0 +1,28 @@
+<?php
+
+class Profile extends Controller {
+	
+    public function index() {
+    	$user=$this->model('Reminders');
+        $this->view('profile/index');
+        if(isset($_POST['update'])){
+
+        	if(empty($_POST['fname'])||empty($_POST['lname'])||empty($_POST['email'])||empty($_POST['pnumber'])||empty($_POST['bdate'])){
+        		header('Location: /home/profile');
+        		dei();
+        	}else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        		header('Location: /home/emailvalidation');
+        		dei();
+        	}else if($user->validate_age($_POST['bdate'],18)==false){
+        		header('Location: /home/validateage');
+        		dei();
+        	}
+        	
+
+        	$user->personalDetails($_POST['fname'],$_POST['lname'],$_POST['email'],$_POST['pnumber'],$_POST['bdate']);
+        	header('location: /home/addedsuccessfully');
+        
+       
+    }
+    }
+}
