1b7ea127c7bc1230368487d049cdc5472ad2f3c9
halsaylani <hasooo7y@hotmail.com>
2017-11-27 03:33:46 -0500

    update
    update the home controller file where 5 function been added for the alerts in the profile page

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,9 +5,44 @@ class Home extends Controller {
     public function index($name = '') {		
         $user = $this->model('User');
 		
-		$message='-welcome '.$_SESSION['name'].'<br>'.'-Today is ' . date("Y/m/d");
+		$message='-welcome '.$_SESSION['name'].'<br>'.'-Today is ' . date("Y/m/d").'<br>'.'-role is '.$_SESSION['role'];
 		
         $this->view('home/index', ['message' => $message]);
+    }
+    public function profile($name = '') {     
+        //$user = $this->model('User');
+        
+        $message='ALL FILEDS ARE REQUIRED';
+        
+        $this->view('profile/index', ['message' => $message]);
+    }
+    public function emailvalidation($name = '') {     
+        //$user = $this->model('User');
+        
+        $message='EMAIL SHOULD BE VALID';
+        
+        $this->view('profile/index', ['message' => $message]);
+    }
+    public function validateage($name = '') {     
+        //$user = $this->model('User');
+        
+        $message='YOU MUST BE OVER 18';
+        
+        $this->view('profile/index', ['message' => $message]);
+    }
+    public function addedsuccessfully($name = '') {     
+        //$user = $this->model('User');
+        
+        $message='UPDATED successfully';
+        
+        $this->view('profile/index', ['successfully' => $message]);
+    }
+    public function profileupdate($name = '') {     
+        //$user = $this->model('User');
+        
+        $message='YOUR PROFILE NEEDE SOME UPDATES';
+        
+        $this->view('remind/index', ['message' => $message]);
     }
     public function admin($name = '') {     
         $user = $this->model('User');
