603f67db547feb058824249a3a03f623cac5e8bf
halsaylani <hasooo7y@hotmail.com>
2017-12-02 02:13:55 -0500

    update the header
    update the header for the admin user where admin can now add manager or staff

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,10 +3,19 @@ if (isset($_SESSION['auth']) != 1) {
     header('Location: /home');
 }
 ?>
-<link href= "/css/style.css" rel="stylesheet" type="text/css"/>
+<head>
+        <link href= "/css/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="icon" href="/favicon.png">
+        <title>COSC 4806</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+    </head>
+
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="/home"><?php echo $_SESSION['name']?></a>
+  <a class="navbar-brand" href="/home"><?php echo $_SESSION['name']?>/</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -23,13 +32,23 @@ if (isset($_SESSION['auth']) != 1) {
       <li class="nav-item">
         <a class="nav-link" href="/report/index">Reports</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/manager/index">Add manager</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="/staff/index">Add staff</a>
+      </li>
       <?php } ?>
+      <li class="nav-item">
+        <a class="nav-link" href="/profile/index">Profile</a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="/logout/index">Logout</a>
       </li>
-    </ul>
-     <li class="nav-item">
-        <a class="nav-link" href="/profile/index">Profile</a>
-      </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" placeholder="Search" type="text">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
   </div>
 </nav>
