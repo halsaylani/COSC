bf6c2a78084c2195c8d2cdf57af1877799f61654
halsaylani <hasooo7y@hotmail.com>
2017-12-02 02:13:01 -0500

    update
    update login and registr page

--- app/views/home/login.php
+++ app/views/home/login.php
@@ -1,22 +1,28 @@
 <?php require_once '../app/views/templates/headerPublic.php' ?>
 <html>
+
 <body>
 <form method="post" action="login/index">
   <fieldset>
     <h1>welcome to the website</h1>
     <div class="form-group">
       <label for="exampleUsername">Username</label>
-      <input class="form-contro" placeholder="Username" type="username" name="name">
+      <div class="col-lg-3">
+      <input class="form-control" placeholder="Username" type="username" name="name" required>
     </div>
+  </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
-      <input class="center-control" placeholder="Password" type="password" name="pass">
+      <div class="col-lg-3">
+      <input class="form-control" placeholder="Password" type="password" name="pass" required>
     </div>
+  </div>
     <button type="submit" name = "submit" class="btn btn-primary">Login</button>
     <a href= "/login/register"> Create an account</a>
   </fieldset>
 </form>
 </body>
+
 </html>
 
     <?php require_once '../app/views/templates/footerPublic.php' ?>
--- app/views/home/register.php
+++ app/views/home/register.php
@@ -5,13 +5,17 @@
       <fieldset>
     <h1>sing up here</h1>
     <div class="form-group">
-      <label for="exampleInputUsername">Username</label>
-      <input class="form-cotrol" placeholder="Username" type="username" name="name">
+      <label for="exampleInputUsername" > Username</label>
+      <div class="col-lg-3">
+      <input class="form-control" placeholder="Username" type="username" name="name" required>
+    </div>
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
-      <input class="fom-control" placeholder="Password" type="password" name="pass">
+      <div class="col-lg-3">
+      <input class="form-control" placeholder="Password" type="password" name="pass" required>
     </div>
+  </div>
     <button type="submit" name = "rig" class="btn btn-primary">Register</button>
     <a href= "../app/views/login.php"> login</a>
   </fieldset>
